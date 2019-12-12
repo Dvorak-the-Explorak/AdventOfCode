9bb38aeac9cb65c7bc78807ab8934aa354291ec5
Joe Teague <joeteague@live.com>
2019-12-12 16:54:43 +1000

    Optimised to only iterate over the increasing numbers

--- 2019/04/solution.hs
+++ 2019/04/solution.hs
@@ -1,20 +1,26 @@
 main = interact $
     show . solve . map read . words
 
-
--- exhaustive check from start to finish
+-- exhaustive check from start to finish - using `next` to iterate (next must be strictly increasing)
 solve :: [Int] -> Int
-solve (a:b:xs) = length [x | x <- [a..b], valid x]
-solve xs = -1
+solve (a:b:xs) = length $ filter valid $ takeWhile (<= b) (iterate next a)
+
+next :: Int -> Int
+next n | nxs == [] = 1000000
+       | otherwise = squash $ (take 6) $ nxs
+         where nxs = nextList $ unsquash n
+-- next = (+) 1
 
 
--- -- part 1
--- valid :: Int -> Bool
--- valid n = n<1000000 &&  increasing (xs) && hasMultiple(xs)
---     where
---         xs = unsquash(n)
+nextList :: [Int] -> [Int]
+nextList [] = []
+nextList (a:xs) | nxs /= [] = a:nxs 
+                | a < 9 = (a+1):(iterate id (a+1))
+                | otherwise = []
+                  where nxs = nextList xs
 
--- -- part 2
+
+-- change hasDouble to hasMultiple for part 1
 valid :: Int -> Bool
 valid n = n<1000000 &&  increasing (xs) && hasDouble(xs)
     where
@@ -23,19 +29,17 @@ valid n = n<1000000 &&  increasing (xs) && hasDouble(xs)
 
 -- has exactly 2 of some value
 hasDouble :: [Int] -> Bool
-hasDouble xs = any ((2 ==) . (flip countVal xs)) xs
--- hasDouble xs = any (\x -> (countVal x xs) == 2) xs
+hasDouble xs = any ((2 ==) . (flip countVal xs)) xs -- flip lets you compose with the first (instead of second) argument
 
 -- has at least 2 of some value
 hasMultiple :: [Int] -> Bool
 hasMultiple xs = any ((2 <=) . (flip countVal xs)) xs
--- hasMultiple xs = any ((\x -> countVal x xs) >= 2) xs
 
 -- how many of a value in a list
 countVal :: Eq a => a -> [a] -> Int
 countVal x xs = length $ filter ((==) x) xs
 
--- is the list (non-strictly) increasing
+-- is the list (non-strictly) increasing.  (Checks if it's sorted)
 increasing :: [Int] -> Bool
 increasing [] = True
 increasing (x:[]) = True
@@ -44,14 +48,8 @@ increasing (x1:x2:xs) = x1 <= x2 && increasing (x2:xs)
 
 
 
--- turn lists of digits into ints, and vice-versa
+-- Turn Int into list of digits
 -- ============================================
-addDigit :: Int -> Int -> Int
-addDigit a b = 10*a + b
-
-squash :: [Int] -> Int
-squash = foldl addDigit 0
-
 
 unsquash :: Int -> [Int]
 unsquash n = tail (preUnsquash [n])
@@ -60,7 +58,12 @@ unsquash n = tail (preUnsquash [n])
         preUnsquash (0:xs) = 0:xs
         preUnsquash (n:xs) = preUnsquash ((div n 10):(mod n 10):xs)
 
+-- un-unsquash
+addDigit :: Int -> Int -> Int
+addDigit a b = 10*a + b
 
+squash :: [Int] -> Int
+squash = foldl addDigit 0
 
 
 
@@ -75,9 +78,5 @@ unsquash n = tail (preUnsquash [n])
 --                  | valid a = (c+1):(next a):b:[]
 --                  | otherwise = c:(next a):b:[]
 -- count xs = xs
-
--- -- just goes to the next one
--- next :: Int -> Int
--- next = (+) 1
 
 
