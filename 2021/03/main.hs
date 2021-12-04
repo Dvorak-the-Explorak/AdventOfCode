9012ec01e452f49e9318f2142edd24ec0141eeee
joe <joeteague@live.com>
2021-12-04 18:48:28 +1000

    Minor changes, renamed some things

--- 2021/03/main.hs
+++ 2021/03/main.hs
@@ -8,6 +8,7 @@ main = interact $
 
 
 solvePart1 :: [String] -> Int
+solvePart1 [] = -1
 solvePart1 input = gamma * epsilon
   where
     gamma = binToInt $ commonBits
@@ -22,15 +23,14 @@ solvePart2 input = oxygen * co2
     oxygen = binToInt $ recurseFilter filterOxygen 0 input
     co2 = binToInt $  recurseFilter filterCo2 0 input
 
+    -- Filter by the nth bit
     filterOxygen n xs = filter (\x -> (x !! n) == (majority (map (!!n) xs))) xs
     filterCo2 n xs = filter (\x -> (x !! n) /= (majority (map (!!n) xs))) xs
 
+    -- Sequence the filters (filter on bit 0 then bit 1 ...)
     recurseFilter f n [] = error "No single item left in filtering process" 
     recurseFilter f n [x] = x
     recurseFilter f n xs = recurseFilter f (n+1) $ f n xs
-
-
-
 
 
 ttrace x = trace (show x) x
@@ -43,7 +43,9 @@ majority :: String -> Char
 majority bin = if moreOnes bin then '1' else '0'
 
 moreOnes :: String -> Bool
-moreOnes bin = (>=0) $ foldl' f 0 bin
+moreOnes bin = (>=0) $ foldl' tally 0 bin
   where
-    f n '1' = n+1
-    f n '0' = n-1 
+    tally n '1' = n+1
+    tally n '0' = n-1 
+    -- If you're feeling silly
+    -- tally n b = n - 1 + (digitToInt b)*2
