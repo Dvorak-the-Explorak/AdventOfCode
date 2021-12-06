f90364775498a4c919f69c144d7ef59ef850e0d8
Joe Teague <joeteague@live.com>
2021-12-06 16:48:11 +1000

    Solved day 06

---
+++ 2021/06/main.hs
@@ -0,0 +1,22 @@
+import Data.List (foldl')
+import Data.List.Split
+
+part1 = False
+
+main = interact $
+  show . solve . (map read) . splitOn ","
+
+solve :: [Int] -> Int
+solve input = sum $ (iterate step counts !! (if part1 then 80 else 256))
+  where 
+    counts = foldl' (\ acc n -> modifyIndex n (+1) acc) [0,0,0,0,0,0,0,0,0] input
+
+step :: [Int] -> [Int]
+step (x:xs) = modifyIndex 6 (+x) $ take 8 xs ++ [x]
+
+modifyIndex :: Int -> (a -> a) -> [a] -> [a]
+modifyIndex n f xs
+  | length (take (n+1) xs) < n+1 = xs
+  | otherwise                 = take n xs ++ [(f $ xs !! n)] ++ drop (n+1) xs 
+
+
