c7602589c1f3310b9f605e75f3f271de10e34e2a
Joe Teague <joeteague@live.com>
2021-12-06 16:50:41 +1000

    Minor refactor

--- 2021/06/main.hs
+++ 2021/06/main.hs
@@ -9,13 +9,13 @@ main = interact $
 solve :: [Int] -> Int
 solve input = sum $ (iterate step counts !! (if part1 then 80 else 256))
   where 
-    counts = foldl' (\ acc n -> modifyIndex n (+1) acc) [0,0,0,0,0,0,0,0,0] input
+    counts = foldl' (\ acc n -> modifyAt n (+1) acc) [0,0,0,0,0,0,0,0,0] input
 
 step :: [Int] -> [Int]
-step (x:xs) = modifyIndex 6 (+x) $ take 8 xs ++ [x]
+step (x:xs) = modifyAt 6 (+x) $ take 8 xs ++ [x]
 
-modifyIndex :: Int -> (a -> a) -> [a] -> [a]
-modifyIndex n f xs
+modifyAt :: Int -> (a -> a) -> [a] -> [a]
+modifyAt n f xs
   | length (take (n+1) xs) < n+1 = xs
   | otherwise                 = take n xs ++ [(f $ xs !! n)] ++ drop (n+1) xs 
 
