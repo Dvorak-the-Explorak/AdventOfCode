e820467eacfaa1719ce6ebf3e4647a3331ba706d
Joe Teague <joeteague@live.com>
2021-12-07 23:19:13 +1000

    Found local instead of global minimum, since it seems to be unique

--- 2021/07/main.hs
+++ 2021/07/main.hs
@@ -9,10 +9,19 @@ part1 = False
 main = interact $
   show . solve1 . (map read) . splitOn ","
 
+ttrace x = trace (show x) x
+
 solve1 :: [Int] -> Int
-solve1 xs = minimum $ map fuel [(minimum xs)..(maximum xs)]
+solve1 xs = localMin $ map fuel [(minimum xs)..(maximum xs)]
   where
     fuel goal = sum $ map (triangle . abs . subtract goal) xs
     triangle n = if part1 
                   then n 
                   else n*(n+1) `div` 2
+
+-- Pretty sure 2nd deriv of `fuel goal` is increasing - works
+localMin :: Ord a => [a] -> a
+localMin [] = error "localMin called on empty list"
+localMin [x] = x
+localMin (x:y:xs) | x < y = x
+                  | otherwise = localMin (y:xs) 
