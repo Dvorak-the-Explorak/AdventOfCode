221b607fadea211ac29a3195c9140effaab7826a
Joe Teague <joeteague@live.com>
2021-12-08 00:11:45 +1000

    Generalised ThiccIO to take any Show Monoid instead of only String

--- util/ThiccIO.hs
+++ util/ThiccIO.hs
@@ -2,10 +2,10 @@ import Control.Monad.IO.Class
 import Control.Monad (liftM, ap)
 import Debug.Trace
 
-newtype ThiccIO a = ThiccIO (String -> IO (String,a))
+newtype ThiccIO s a = ThiccIO (s -> IO (s,a))
 
 -- Sequence the two operations together
-instance Monad ThiccIO where
+instance Monad (ThiccIO s) where
   return x = ThiccIO $ \buff -> return (buff, x)
   ThiccIO f1 >>= getNext = 
     ThiccIO $ \buff1 -> do
@@ -13,34 +13,44 @@ instance Monad ThiccIO where
                 let ThiccIO f2 = getNext x1
                 f2 buff2
 
-instance Functor ThiccIO where
+instance Functor (ThiccIO s) where
   fmap = liftM
 
-instance Applicative ThiccIO where
+instance Applicative (ThiccIO s) where
   pure = return
   (<*>) = ap
 
 
 -- IO operations flush the buffer
-instance MonadIO ThiccIO where
+instance (Show s, Monoid s) => MonadIO (ThiccIO s) where
   liftIO op = ThiccIO $ \buff -> do
-                            putStr buff
+                            putStr $ show buff
                             x <- op
-                            return ("", x)
+                            return (mempty, x)
 
 -- running it as IO flushes the buffer at the end
-runThicc :: ThiccIO a -> IO a
+runThicc :: (Show s, Monoid s) => ThiccIO s a -> IO a
 runThicc (ThiccIO f) = do 
-  (buff, x) <- f ""
-  putStr buff
+  (buff, x) <- f mempty
+  putStr $ show buff
   return x
 
-thiccPutStr :: String -> ThiccIO ()
-thiccPutStr x = ThiccIO $ \buff -> return (buff++x, ())
+thiccPutStr :: Thickness -> ThiccIO Thickness ()
+thiccPutStr x = ThiccIO $ \buff -> return (buff <> x, ())
+
+
+newtype Thickness = Thickness String
+instance Semigroup Thickness where
+  Thickness x <> Thickness y = Thickness $ x++y
+instance Monoid Thickness where
+  mempty = Thickness ""
+  mappend = (<>)
+instance Show Thickness where
+  show (Thickness x) = x
 
 main = runThicc $ do
   liftIO $ putStr "<"
-  thiccPutStr "Hello"
+  thiccPutStr $ Thickness "Hello"
   liftIO $ putStrLn ">"
 
 
