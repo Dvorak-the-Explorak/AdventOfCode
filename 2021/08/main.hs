e6b0f655fc55e3f1462dc28bf85a2dac180af338
Joe Teague <joeteague@live.com>
2021-12-10 09:52:24 +1000

    Added logical solution for 8.2, using lots of partial functions

--- 2021/08/main.hs
+++ 2021/08/main.hs
@@ -1,8 +1,9 @@
 import Text.ParserCombinators.Parsec hiding (State)
 import Text.Parsec.Char
-import Data.List (foldl', permutations, sort, elemIndex)
+import Data.List (foldl', permutations, sort, elemIndex, delete)
 import Data.Char (chr, ord)
 import qualified Data.HashMap.Strict as Map
+import Data.Hashable (Hashable)
 
 import Debug.Trace
 
@@ -20,10 +21,61 @@ main = do
           let perm = "abcdegf"
 
           -- print $ map (runPerm perm) outputs
-          let outputs = map solveSegment segments
+          let outputs = map solveSegment2 segments
           print $ sum outputs
 
 countPred p xs = foldl' (\ acc x -> if p x then acc+1 else acc) 0 xs
+
+solveSegment2 :: ([String], [String]) -> Int
+solveSegment2 (raw_inputs, outputs) = result
+  where
+    inputs = map sort raw_inputs
+    one = head $ filter (lengthIs 2) inputs
+    four = head $ filter (lengthIs 4) inputs 
+    seven = head $ filter (lengthIs 3) inputs 
+    eight = head $ filter (lengthIs 7) inputs 
+
+    letterCounts = getCounts $ concat inputs
+    b_enc = fst $ head $ Map.toList $ Map.filter (==6) letterCounts
+    e_enc = fst $ head $ Map.toList $ Map.filter (==4) letterCounts
+    f_enc = fst $ head $ Map.toList $ Map.filter (==9) letterCounts
+    c_enc = head $ delete f_enc one
+
+
+    -- remove the solved ones
+    inputs' = foldl' (\acc x -> delete x acc) inputs [one, four, seven, eight]
+
+    zero = head $ filter (\x -> all (flip elem x) [b_enc,e_enc,f_enc,c_enc]) inputs'
+    six = head $ filter (\x -> not (elem c_enc x) && all (flip elem x) [b_enc,e_enc,f_enc]) inputs'
+    two = head $ filter (elem e_enc &&& (not . elem f_enc) &&& (not . elem b_enc)) inputs'
+    three = head $ filter (elem f_enc &&& (not . elem e_enc) &&& (not . elem b_enc)) inputs'
+    five = head $ filter (lengthIs 5 &&& elem f_enc &&& (not . elem e_enc) &&& elem b_enc) inputs'
+    nine = head $ filter (lengthIs 6 &&& elem f_enc &&& (not . elem e_enc) &&& elem b_enc) inputs'
+
+    lengthIs n = (==n) . length
+
+    valMap = Map.fromList $ [(zero,0),(one,1),(two,2),(three,3),(four,4),(five,5),(six,6),(seven,7),(eight,8),(nine,9)]
+
+    getJust Nothing = error $ "Uh OH"
+    getJust (Just x) = x
+
+    result = digitsToInt $ map (getJust . \x -> Map.lookup x valMap) $ map sort outputs
+
+
+
+
+
+
+(&&&) :: (a -> Bool) -> (a -> Bool) -> (a -> Bool)
+(&&&) p q x = (p x) && (q x)
+
+
+
+getCounts :: (Eq a, Hashable a) => [a] -> Map.HashMap a Int
+getCounts = foldl' (\ acc x -> tally x acc) $ Map.fromList []
+  where
+    tally x = Map.insertWith (+) x 1
+
 
 solveSegment :: ([String], [String]) -> Int
 solveSegment (inputs, outputs) = result
