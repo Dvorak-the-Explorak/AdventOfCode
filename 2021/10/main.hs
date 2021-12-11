534736f93880c11273106916a4941cbed6611876
Joe Teague <joeteague@live.com>
2021-12-12 06:47:05 +1000

    Solved 10.1 without parsec

--- 2021/10/main.hs
+++ 2021/10/main.hs
@@ -1,23 +1,28 @@
-import Text.ParserCombinators.Parsec hiding (State)
-import Text.Parsec.Char
+
 import qualified Data.HashMap.Strict as Map
 
-import Debug.Trace
+main = interact $ 
+  show . solve . lines
 
-main = do
-  input <- getContents
-  let result = parse allLines "(unknown)" input
-  case result of
-    (Left err) -> print err
-    (Right scores) -> print $ sum scores
+solve :: [String] -> Int
+solve input = result
+  where
+    result = sum $ map (corruptScore "") input
 
-line :: Parser Int
-line = chunks <* restOfLine
+corruptScore :: String -> String -> Int
+corruptScore (c:cs) (x:xs)
+    | x == c = corruptScore cs xs
+corruptScore cs (x:xs) 
+    | x `elem` "({[<" = corruptScore (getClose x : cs) xs
+    | otherwise       = closeValue x
+corruptScore _ [] = 0
 
-allLines = (eof >> return []) <|> do
-  score <- line 
-  scores <- allLines
-  return (score:scores)
+closeValue ')' = 3
+closeValue ']' = 57
+closeValue '}' = 1197
+closeValue '>' = 25137
+closeValue _ = 0
+
 
 closeMap :: Map.HashMap Char Char
 closeMap = Map.fromList [('(', ')'),
@@ -28,37 +33,3 @@ getClose :: Char -> Char
 getClose open = fromMaybe $ Map.lookup open closeMap
 
 fromMaybe (Just x) = x
-
--- restOfLine = manyTill anyChar endOfLine
-restOfLine = many (noneOf "\n") >> endOfLine
-
-
-chunks = do
-  score <- chunk <|> return (-1)
-  case score of
-    -1 -> return 0 -- no chunk
-    0 -> chunks -- found a valid chunk, keep going
-    x -> return x
-
-
-chunk = do
-  open <- oneOf "({[<"
-  let close = (char (getClose open) >> return 0)
-                  <|> corrupt
-  score <- chunks
-  case score of
-    (-1) -> close
-    0 -> close
-    x -> return x
-
-
-corrupt :: Parser Int
-corrupt = (char ')' >> return 3) <|> 
-          (char ']' >> return 57) <|>
-          (char '}' >> return 1197) <|>
-          (char '>' >> return 25137) <|>
-          (lookAhead endOfLine >> return 0) <|>
-          (lookAhead eof >> return 0) <?> "Corruption"
-
-
-ttrace x = trace (show x) x
