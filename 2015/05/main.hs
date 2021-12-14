dc6688f301d3a5deb5ff45dfa83f95e3ab05424b
joe <joeteague@live.com>
2021-12-15 00:26:14 +1000

    Solved 2015 5.2 (wihtout regex)

--- 2015/05/main.hs
+++ 2015/05/main.hs
@@ -3,6 +3,8 @@ import Helpers (pairs)
 
 import Data.Semigroup
 
+import qualified Data.HashMap.Strict as Map
+type Map = Map.HashMap
 
 -- example
 type PuzzleInput = [String]
@@ -21,22 +23,42 @@ main = do
   let result1 = solve2 vals
   print result1
 
+
 getPuzzleInput :: IO PuzzleInput
 getPuzzleInput = lines <$> getContents 
 
 
 -- example
 solve1 :: PuzzleInput-> Int
-solve1 = length . filter valid
+solve1 = length . filter valid1
 
 -- example
 solve2 :: PuzzleInput -> Int
-solve2 = const 0
+solve2 = length . filter valid2
 
 
+allP preds = getAll . (mconcat $ map (fmap All) preds)
 
-valid :: String -> Bool
-valid = getAll . (mconcat $ map (fmap All) [hasDouble, threeVowels, noProhibited])
+
+valid1 :: String -> Bool
+valid1 = allP [hasDouble, threeVowels, noProhibited]
+
+valid2 = allP [hasPairTwice, hasNearDouble]
+
+-- Wish I had some of that regex
+hasPairTwice :: String -> Bool
+hasPairTwice s = any secondPair [0..(length s)-2]
+  where
+    secondPair n = (pair n `isInfixOf` prefix n) || (pair n `isInfixOf` suffix n)
+    pair n = take 2 $ drop n s
+    prefix n = take n s
+    suffix n = drop (n+2) s
+
+hasNearDouble :: String -> Bool
+hasNearDouble s = any nearDouble [0..(length s)-3]
+  where
+    nearDouble n = (s!!n) == (s!!(n+2)) && (s!!n) /= (s!!(n+1))
+
 
 hasDouble :: String -> Bool
 hasDouble = not . null . filter (\(x,y) -> x==y) . pairs
