f79a0a43596893c80a9cf826feddfb459bd08f4d
joe <joeteague@live.com>
2021-12-21 00:35:37 +1000

    Solved 2021 day 20 with pixel colours relative to the infinite sea

--- 2021/20/main.hs
+++ 2021/20/main.hs
@@ -44,7 +44,7 @@ getPuzzleInput = do
 
   return (V.fromList encoding, pad '0' $ Grid input)
 
--- solve1 :: PuzzleInput -> Grid a
+-- solve1 :: PuzzleInput -> Int
 solve1 (enc, input) = num1s
   where 
     num1s = sum $ map (length . filter (=='1')) $ getGrid grid'
@@ -58,26 +58,44 @@ solve2 (enc, input) = num1s
 
 
 
-
-
-
-
-
-
-
 charToBin :: Char -> Char
 charToBin '#' = '1'
 charToBin '.' = '0'
 
-decodeImage :: Encoding -> Char -> Grid Char -> Grid Char
-decodeImage enc sea grid = mapKernel (decodeKernel sea enc) $ pad sea grid
+decodeImage :: Encoding -> Grid Char -> Grid Char
+decodeImage enc grid = mapKernel (decodeKernel enc) $ pad '0' grid
+
+
+naiveStep :: Int -> Encoding -> Grid Char -> Grid Char
+naiveStep 0 enc g = g
+naiveStep n enc g = naiveStep (n-1) enc $ decodeImage enc g
+
+
+-- reverses the encoding every step, as each step the board silently flips
+toggleStep :: Int -> Encoding -> Grid Char -> Grid Char
+toggleStep 0 enc g = g
+toggleStep n enc g = toggleStep (n-1) (V.reverse $ flipEncoding enc) $ decodeImage enc g
+
 
 step :: Int -> Encoding -> Grid Char -> Grid Char
 step 0 enc g = g
-step n enc g = step (n-1) enc $ decodeImage enc sea g
--- step n enc g = step (n-1) enc $ pad sea g
-  where
-    sea = getIndex (0,0) g
+step n enc g = case (V.head enc, V.last enc) of
+  -- sea always 0, no modifications
+  ('0', _) -> naiveStep n enc g
+  -- sea alternates between 0 and 1
+  ('1', '0') -> (if n `mod` 2 == 1 then flipGrid else id)  $ toggleStep (n-1) (V.reverse enc) $ decodeImage (flipEncoding enc) g
+  -- sea stays at 1.  
+  ('1', '1') -> undefined -- not sure if the line below is accurate
+  -- ('1', '1') -> flipGrid $ naiveStep (n-1) (V.reverse $ flipEncoding enc) $ decodeImage (flipEncoding enc) g
+
+
+flipEncoding :: Vec Char -> Vec Char
+flipEncoding = V.map flipPixel
+
+flipGrid = fmap flipPixel
+
+flipPixel '0' = '1'
+flipPixel '1' = '0'
 
 pad :: a -> Grid a -> Grid a
 pad val grid@(Grid vals) = (Grid $ [newRow] ++ vals' ++ [newRow])
@@ -87,11 +105,11 @@ pad val grid@(Grid vals) = (Grid $ [newRow] ++ vals' ++ [newRow])
     (_,n) = gridDim grid
 
 -- assume grid is padded with 1 layer of the "infinite sea" value
-decodeKernel :: Char -> Encoding -> Kernel Char Char
-decodeKernel sea enc = \getVal coord -> (enc !) $ indexKernel sea getVal coord
+decodeKernel :: Encoding -> Kernel Char Char
+decodeKernel enc = \getVal coord -> (enc !) $ indexKernel getVal coord
 
-indexKernel :: Char ->  Kernel Char Int
-indexKernel sea = \getVal coord -> binToInt $ map (maybe sea id . getVal) $ squareKernel coord
+indexKernel :: Kernel Char Int
+indexKernel = \getVal coord -> binToInt $ map (maybe '0' id . getVal) $ squareKernel coord
 
 binToInt :: String -> Int
 binToInt = foldl' (\acc x -> acc*2 + digitToInt x) 0
