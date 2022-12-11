a8227ae9e04ce4311390276fb90202675bb4bcf2
Joe Teague <joeteague@live.com>
2022-12-12 01:06:56 +1000

    2022 Solved day 09

---
+++ 2022/09/main.hs
@@ -0,0 +1,100 @@
+import Data.List (foldl')
+-- import Helpers (chain)
+import Control.Monad.State
+import qualified Data.Set as Set
+import Control.Applicative (liftA2)
+
+import Debug.Trace (trace)
+ttrace x = trace (show x) x
+
+-- example
+type PuzzleInput = [Instr]
+type Instr = (Char, Int)
+type Loc = (Int, Int)
+type Hist = Set.Set Loc
+
+
+
+
+part1 = True
+
+main = do
+  vals <- getPuzzleInput
+
+  putStr "Part 1: "
+  let result1 = solve1 vals
+  print result1
+
+  putStr "Part 2: "
+  let result2 = solve2 vals
+  print result2
+
+getPuzzleInput :: IO PuzzleInput
+getPuzzleInput = map (\x -> (x!!0, read $ drop 2 x)) <$> lines <$> getContents 
+
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (x, y) = (f x, y)
+
+step :: Instr -> State ([Loc] , Hist) ()
+step (_, 0) = return ()
+step (dir, n) = do
+  -- Get the first knot
+  (x,y) <- gets $ head . fst 
+  -- Move the first knot to newLoc according to one step of the instruction
+  let newLoc = case dir of 
+                'U' -> (x, y+1)
+                'D' -> (x, y-1)
+                'L' -> (x-1, y)
+                'R' -> (x+1, y)
+  -- Replace the head of the list of knots
+  modify $ first $ (newLoc:) . tail
+  -- Run physics
+  modify $ first $ cascade update
+  -- Update 
+  modify $ updateHistory
+  step (dir, n-1)
+
+updateHistory :: ([Loc], Hist) -> ([Loc], Hist)
+updateHistory (vals, hist) = (vals, Set.insert (last vals) hist)
+
+update :: Loc -> Loc -> Loc
+update p@(px,py) c@(x,y) = if dist p c <= 1 then (x,y) else (x', y')
+  where
+    x' = x + signum (px - x)
+    y' = y + signum (py - y)
+
+dist :: Loc -> Loc -> Int
+dist (x1,y1) (x2,y2) = max (abs $ x1 - x2) (abs $ y1 - y2)
+
+-- cascade :: (a -> a -> a) -> [a] -> [a]
+-- cascade _ [] = []
+-- cascade _ [x] = [x]
+-- cascade f (p:x:xs) = p:cascade f ((f p x):xs)
+
+cascade :: (a -> a -> a) -> [a] -> [a]
+cascade _ [] = []
+cascade f (x:xs) = reverse $ foldl' acc [x] xs
+  where
+    --  call f on latest output and next value, 
+    --  add the result to list of outputs
+    -- acc prevs x = f (head prevs) x : prevs
+     --acc prevs = (:prevs) . f (head prevs)
+    -- acc = liftA2 (.) (flip (:)) (f . head)
+    acc = liftA2 fmap (flip (:)) $ f . head -- fucking lol
+
+-- cascade :: (a -> a -> a) -> [a] -> [a]
+-- cascade f [] = []
+-- cascade f (x:xs) = x : zipWith f (cascade f (x:xs)) xs
+
+
+
+solve1 :: PuzzleInput-> Int
+solve1 instrs = evalState (mapM step instrs >> gets (Set.size . snd)) initialState
+  where 
+    initialState = (take 2 $ repeat (0,0), Set.fromList [])
+
+
+solve2 :: PuzzleInput -> Int
+solve2 instrs = evalState (mapM step instrs >> gets (Set.size . snd)) initialState
+  where 
+    initialState = (take 20 $ repeat (0,0), Set.fromList [])
---
+++ 2022/09/test.py
@@ -0,0 +1,63 @@
+
+# I don't have a haskell equivalent
+def iterative(f, vals):
+	result = vals[:1]
+	for i in range(1, len(vals)):
+		result.append(f(result[i-1], vals[i]))
+	return result
+
+	# # or:
+	# vals = vals[:]
+	# for i in range(1, len(vals)):
+	# 	vals[i] = f(vals[i-1], vals[i])
+	# return vals
+
+
+# cascade :: (a -> a -> a) -> [a] -> [a]
+# cascade _ [] = []
+# cascade _ [x] = [x]
+# cascade f (p:x:xs) = p:cascade f ((f p x):xs)
+def recursive(f, vals):
+	# Keeps the previous result at the front of the list,
+	#	so apply f to the first two elems, using the result
+	#	as the new second element.
+	#	The first element doesn't need to be passed along in
+	#	the recursive call.
+	if len(vals) < 2:
+		return vals[:]
+	prev,current,*rest = vals
+	result = f(prev, current)
+	return [prev] + recursive(f, [result] + rest)
+
+# cascade :: (a -> a -> a) -> [a] -> [a]
+# cascade f [] = []
+# cascade f (x:xs) = x : zipWith f (cascade f (x:xs)) xs
+def lazyRecursive(f, vals):
+	# The idea is just take the answer and a copy of the input shifted forwards (first element removed),
+	#	then apply the function to corresponding elements.  
+	# The elements in the shifted copy will be the second argument, 
+	#	and the first argument will be the result at the previous element
+
+	if not vals:
+		return []
+
+	# This solution requires a lazily evaluated generator,
+	#	so this is here so I can wrap it in a list at the end
+	def gen():
+		# return the head of the list unmodified
+		yield vals[0]
+
+		# zip together the full generator with the tail of the values
+		yield from (f(*pair) for pair in zip(gen(), vals[1:]))
+
+	return list(gen())
+
+def add(x, y):
+	return x + y
+
+
+N = 400
+print(iterative(add, [1]*N))
+print(recursive(add, [1]*N))
+print(lazyRecursive(add, [1]*N))
+
