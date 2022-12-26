99c2d13e4a58608efe32c712fd55ce4de8e3bf83
Joe Teague <joeteague@live.com>
2022-12-26 20:21:50 +1000

    2022 Added runBfs wrapper, minor cleanup

--- 2022/12/main.hs
+++ 2022/12/main.hs
@@ -32,22 +32,44 @@ main = do
   let result2 = solve2 vals
   print result2
 
-bfs :: (Coord -> Context [Coord]) -> 
-        (Coord -> Context Bool) -> 
-        Set Coord -> 
-        [(Int, Coord)] -> 
-        Context (Maybe Int)
+solve1 :: PuzzleInput -> Maybe Int
+solve1 (start, end, grid) = runBfs climbUp (return . (==end)) start grid
+
+solve2 :: PuzzleInput -> Maybe Int
+solve2 (_, end, grid) = runBfs climbDown (fmap (==0) . getVal) end grid
+
+
+-- Wrapper
+runBfs :: (Coord -> Context [Coord]) -> -- Get adjacent coords
+        (Coord -> Context Bool) ->      -- Is end coordinate
+        Coord ->                        -- start
+        Grid ->                         -- the grid
+        Maybe Int                       -- result (possibly no path to goal)
+runBfs adj isEnd start grid = runReader go grid
+  where go = bfs adj isEnd Set.empty [(0,start)]
+
+
+bfs :: (Coord -> Context [Coord]) ->  -- Get adjacent coords
+        (Coord -> Context Bool) ->    -- Is end coordinate
+        Set Coord ->                  -- visited
+        [(Int, Coord)] ->             -- queue of (dist, coord to visit)
+        Context (Maybe Int)           -- result (possibly no path to goal)
 bfs _ _ _ [] = return Nothing
-bfs adj end done ((dist,curr):todo) = do
-  found <- end curr
+bfs adj isEnd done ((dist,curr):todo) = do
+  -- Is curr the end of the search
+  finished <- isEnd curr
+
+  -- Next coords to search
   next <- filter (not . (`elem` done)) <$> adj curr
+
+  -- Updated params after visiting current
   let done' = Set.insert curr done
   let todo' = (todo ++) $ map (dist+1,) next
 
-  if found then return (Just dist) else
+  if finished then return (Just dist) else
     if (curr `elem` done) 
-      then bfs adj end done todo
-      else bfs adj end done' todo'
+      then bfs adj isEnd done todo
+      else bfs adj isEnd done' todo'
 
 climbUp :: Coord -> Context [Coord]
 climbUp = adjacent valid 
@@ -63,15 +85,7 @@ climbDown = adjacent valid
     valid coord next = do
       currentHeight <- getVal coord
       nextHeight <- getVal next
-      return (nextHeight + 1 >= currentHeight)
-
-solve1 :: PuzzleInput -> Maybe Int
-solve1 (start, end, grid) = runReader go grid
-  where go = bfs climbUp (return . (==end)) Set.empty [(0,start)]
-
-solve2 :: PuzzleInput -> Maybe Int
-solve2 (_, end, grid) = runReader go grid
-  where go = bfs climbDown (fmap (==0) . getVal) Set.empty [(0,end)]
+      return (currentHeight <= 1 + nextHeight)
 
 -- so slow lol
 -- minimum of BFS distance from each start point
@@ -84,6 +98,8 @@ solve2_old (_, end, grid) = minimum $ catMaybes $ map (\x -> solve1 (x,end,grid)
     starts = concat $ zipWith (\r cs -> map (r,) cs) startRows startCols
 
 -- ========================================================================================
+
+-- `Reader Grid` helpers
 
 inGrid :: Coord -> Context Bool
 inGrid (r, c) = do
